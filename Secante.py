d55db0ab66b48d94ca4e4779d2d971572f7914e1
João Vítor Fonseca de Mendonça <joovitor177@gmail.com>
2020-09-20 13:03:50 -0300

    Update Secante.py

--- Secante.py
+++ Secante.py
@@ -1,7 +1,6 @@
 import math
 
-def f(x): # Função do Exercício
-  return math.pow(x,3)-1.7*math.pow(x,2)- 12.78*x -10.08 
+f = lambda x: math.pow(x,3)-1.7*math.pow(x,2)- 12.78*x -10.08 # funcao f
 
 x0 = 4 # Chute inicial 1
 x1 = 15 # Chute inicial 2
